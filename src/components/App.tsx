b60baff51aa324e7e814d76a7fe9ba1fda3b6914
an1mus <1stallways@gmail.com>
2020-01-21 20:51:43 +0200

    Components structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Router, Switch} from 'react-router-dom';
 import styled from 'styled-components';
 import {history} from '../commons';
+import './App.css';
 
 const AppContainer = styled.div``;
 
