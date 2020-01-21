672500ad33e0f8dc458c18f180a51086e816ef95
an1mus <1stallways@gmail.com>
2020-01-21 22:14:11 +0200

    Header parts

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import Button from '../button';
+import { ReactComponent as Logo } from '../../../assets/logo.svg';
+
+const LogoContainer = styled.div`
+    height: 4.1rem;
+    width: 4rem;
+    
+    svg {
+        width: 100%;
+        height: 100%;
+    }
+`;
+const NavContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 1rem 2.1rem 0.9rem 1.2rem;
+    align-items: center;
+    justify-content: space-between;
+`;
 
 const Header = () => {
     return (
-        <>
-            Header;
-        </>
+        <NavContainer>
+            <LogoContainer>
+                <Logo/>
+            </LogoContainer>
+            <Button title={'Premium'} click={() => alert('Stub alert')} primary={true}/>
+        </NavContainer>
     )
 };
 
